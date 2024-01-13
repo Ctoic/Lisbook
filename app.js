e0ddc4ae78a634ca0e7387a2dbce3dc3fb39b0d6
ctoic <i202612@nu.edu.pk>
2024-01-13 15:38:29 +0500

    updated app

--- app.js
+++ app.js
@@ -30,34 +30,34 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     document.addEventListener("DOMContentLoaded", function () {
-        const prevBtn = document.getElementById("prev-btn");
-        const nextBtn = document.getElementById("next-btn");
-    
-        // Define the pages
-        const pages = ['index.html', 'genres.html', 'about.html'];
-        let currentPageIndex = pages.indexOf(window.location.pathname);
-    
-        // Function to navigate to the next page
-        function nextPage() {
-            if (currentPageIndex < pages.length - 1) {
-                currentPageIndex++;
-                window.location.href = pages[currentPageIndex];
-            }
+    const prevBtn = document.getElementById("prev-btn");
+    const nextBtn = document.getElementById("next-btn");
+
+    // Define the pages
+    const pages = ['index.html', 'genres.html', 'about.html'];
+    let currentPageIndex = pages.indexOf(window.location.pathname);
+
+    // Function to navigate to the next page
+    function nextPage() {
+        if (currentPageIndex < pages.length - 1) {
+            currentPageIndex++;
+            window.location.href = pages[currentPageIndex];
         }
-    
-        // Function to navigate to the previous page
-        function prevPage() {
-            if (currentPageIndex > 0) {
-                currentPageIndex--;
-                window.location.href = pages[currentPageIndex];
-            }
+    }
+
+    // Function to navigate to the previous page
+    function prevPage() {
+        if (currentPageIndex > 0) {
+            currentPageIndex--;
+            window.location.href = pages[currentPageIndex];
         }
-    
-        // Event listener for "Next" button
-        nextBtn.addEventListener("click", nextPage);
-    
-        // Event listener for "Previous" button
-        prevBtn.addEventListener("click", prevPage);
-    });
-    
+    }
+
+    // Event listener for "Next" button
+    nextBtn.addEventListener("click", nextPage);
+
+    // Event listener for "Previous" button
+    prevBtn.addEventListener("click", prevPage);
+});
+
 });
