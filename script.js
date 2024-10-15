49dbf81eb92a9aa2666b0fcf49df21e2d4d1fbde
YakeDev <erickay.dev@gmail.com>
2024-10-15 22:38:31 +0200

    Add Player controls buttons

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const currentBookId = "7";
   let allBooksList = [];
   let currentBook;
+  // Audio Player Controls Buttons
+  const ctrlPlay = document.getElementById("ctrl-play");
+  const ctrlFastBackward = document.getElementById("fast-backward");
+  const ctrlFastForward = document.getElementById("fast-forward");
+  const volumeUp = document.getElementById("volume-up");
+  const volumeDown = document.getElementById("volume-down");
 
   // Function to display error popup
   function showErrorPopup(message) {
@@ -204,6 +210,35 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  //Audio player Controls Buttons
+  ctrlPlay.addEventListener("click", function () {
+    if (audioPlayer.paused) {
+      ctrlPlay.innerHTML = '<i class="bi bi-pause-fill"></i>';
+      audioPlayer.play();
+    } else {
+      ctrlPlay.innerHTML = '<i class="bi bi-play-fill"></i>';
+      audioPlayer.pause();
+    }
+  });
+
+  ctrlFastForward.addEventListener("click", function () {
+    audioPlayer.currentTime += 30;
+  });
+
+  ctrlFastBackward.addEventListener("click", function () {
+    audioPlayer.currentTime -= 30;
+  });
+  volumeUp.addEventListener("click", function () {
+    if (audioPlayer.volume < 1) {
+      audioPlayer.volume = Math.max(audioPlayer.volume + 0.1, 0);
+    }
+  });
+  volumeDown.addEventListener("click", function () {
+    if (audioPlayer.volume > 0) {
+      audioPlayer.volume = Math.max(audioPlayer.volume - 0.1, 0);
+    }
+  });
+
   // Comment Submission
   if (commentForm) {
     commentForm.addEventListener("submit", function (e) {
@@ -226,20 +261,24 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   // Feedback Submission
-window.onload = function() {
-  document.getElementById('feedback-form').addEventListener('submit', function(event) {
-      event.preventDefault();
-      emailjs.sendForm('your_service_id', 'feedback_form', this)
-          .then(() => {
-              console.log('SUCCESS!');
+  window.onload = function () {
+    document
+      .getElementById("feedback-form")
+      .addEventListener("submit", function (event) {
+        event.preventDefault();
+        emailjs.sendForm("your_service_id", "feedback_form", this).then(
+          () => {
+            console.log("SUCCESS!");
 
-              // Reset the form 
-              document.getElementById("feedback-form").reset();
-          }, (error) => {
-              console.log('FAILED...', error);
-          });
-  });
-}
+            // Reset the form
+            document.getElementById("feedback-form").reset();
+          },
+          (error) => {
+            console.log("FAILED...", error);
+          }
+        );
+      });
+  };
 
   // Theme Toggle
   const body = document.body;
@@ -304,103 +343,9 @@ document.addEventListener("DOMContentLoaded", function () {
   loadHTML("./pages/footer.html", "footer-placeholder");
 });
 
-
-
 // // Hide the loader when the page is fully loaded
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   // Cache le spinner après que la page est entièrement chargée
-  const loader = document.getElementById('loader');
-  loader.classList.add('hidden');
+  const loader = document.getElementById("loader");
+  loader.classList.add("hidden");
 });
-
-
-
-
-// const translations = {
-//   en: {
-//     home: "Home",
-//     about: "About",
-//     topAudioBooks: "Top Audio Books",
-//     by: "By",
-//     favourite: "Save to Favourites",
-//     share: "Share",
-//     view: "View comments",
-//     previousbook : "Previous Book",
-//     nextbook: "Next Book",
-//     share1: "Share Your Comments",
-//     share2: "Share Your Comments",
-//     yourname: "Your Name:",
-//     yourcomment: "Your Comment:",
-//   },
-//   fr: {
-//       home: "Accueil",
-//       about: "À propos",
-//       topAudioBooks: "Meilleurs livres audio",
-//       by: "Par",
-//       favourite: "Enregistrer dans les favoris",
-//       share: "Partager",
-//       view: "Afficher les commentaires",
-//       previousbook : "Livre précédent",
-//       nextbook: "Livre suivant",
-//       share1: "Partagez vos commentaires",
-//       share2: "Partager le commentaire",
-//       yourname: "Votre nom :",
-//       yourcomment: "Votre commentaire :",
-//   },
-//   es: {
-//       home: "Inicio",
-//       about: "Acerca de",
-//       topAudioBooks: "Mejores libros de audio",
-//       by: "Por",
-//       favourite: "Guardar en favoritos",
-//       share: "Compartir",
-//       view: "Ver comentarios",
-//       previousbook : "Libro anterior",
-//       nextbook: "Siguiente libro",
-//       share1: "Comparte tus comentarios",
-//       share2: "Comparte tu comentario",
-//       yourname: "Tu nombre:",
-//       yourcomment: "Tu comentario:",
-//   }
-// };
-
-// // Fonction pour changer la langue
-// function changeLanguage() {
-//   const language = document.getElementById('language').value;
-
-//   // Mettez à jour le localStorage avec la langue choisie
-//   localStorage.setItem('language', language);
-//   updateText(language);
-// }
-
-// // Fonction pour mettre à jour le texte en fonction de la langue choisie
-// function updateText(language) {
-//   if (translations[language]) {
-//       document.getElementById('home').textContent = translations[language].home;
-//       document.getElementById('about').textContent = translations[language].about;
-//       document.getElementById('top-audio-books').textContent = translations[language].topAudioBooks;
-//       document.getElementById('author-gd').textContent = translations[language].by + " Richard Dawkins";
-//       document.getElementById('author-frankestine').textContent = translations[language].by + " Mary Shelby";
-//       document.getElementById('author-sherlock').textContent = translations[language].by + " Sir Arthur Conan Doyle";
-//       document.getElementById('author-sapiens').textContent = translations[language].by + " Yuval Noah Harari";
-//       document.getElementById('author-gd1').textContent = translations[language].by + " Richard Dawkins";
-//       document.getElementById('favourite').textContent = translations[language].favourite;
-//       document.getElementById('share').textContent = translations[language].share;
-//       document.getElementById('view').textContent = translations[language].view;
-//       document.getElementById('previousbook').textContent = translations[language].previousbook;
-//       document.getElementById('nextbook').textContent = translations[language].nextbook;
-//       document.getElementById('share1').textContent = translations[language].share1;
-//       document.getElementById('share2').textContent = translations[language].share2;
-//       document.getElementById('yourname').textContent = translations[language].yourname;
-//       document.getElementById('yourcomment').textContent = translations[language].yourcomment;
-//   } else {
-//       console.error('Langue non disponible');
-//   }
-// }
-
-// // À l'initialisation, récupérez la langue du localStorage et mettez à jour le sélecteur et le texte
-// document.addEventListener('DOMContentLoaded', () => {
-//   const savedLanguage = localStorage.getItem('language') || 'fr'; // Valeur par défaut : français
-//   document.getElementById('language').value = savedLanguage;
-//   updateText(savedLanguage);
-// });
